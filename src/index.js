99b88ada86cf3f68f7517ff4cbad50f87319cfbc
boldCM <carolin@cc-meier.de>
2020-10-20 10:25:40 +0200

    SetUp Loaders

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import _ from "lodash";
+import "./style.css";
+//import Icon/IMg...
+import printMe from "./print";
+// für dem button unten
 
 function component() {
   const element = document.createElement("div");
+  //   const btn = document.createElement('button');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  // Add the image to our existing div.
+  //  const myIcon = new Image();
+  //  myIcon.src = Icon;
+
+  //  element.appendChild(myIcon);
+
+  //   für den button aus printMe...
+  //   btn.innerHTML = 'Click me and check the console!';
+  //   btn.onclick = printMe;
+
+  //   element.appendChild(btn);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,34 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "development",
 
-  entry: "./src/index.js",
+  entry: {
+    app: "./src/index.js",
+    print: "./src/print.js",
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),
+  ],
   output: {
-    filename: "main.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
+    ],
   },
 };
