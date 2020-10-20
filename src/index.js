ac9ccc315cba3783529179324f6c66b926817c8b
boldCM <carolin@cc-meier.de>
2020-10-20 20:44:26 +0200

    Add some Styling

--- src/index.js
+++ src/index.js
@@ -1,29 +1,14 @@
 import "./style.css";
 import { createInputNumber } from "../src/Components/inputNumber";
 import { createSubmitCountdown } from "../src/Components/submitCountdown";
-//import Icon/IMg...
-import printMe from "./print";
-// für dem button unten
+import { createElement } from "../Utils/element";
 
 function component() {
-  const element = document.createElement("div");
-  //   const btn = document.createElement('button');
+  const element = createElement("div", {
+    className: "mainContainer",
+  });
 
-  // Lodash, now imported by this script
-  element.classList.add("hello");
   element.append(createInputNumber(), createSubmitCountdown());
-
-  // Add the image to our existing div.
-  //  const myIcon = new Image();
-  //  myIcon.src = Icon;
-
-  //  element.appendChild(myIcon);
-
-  //   für den button aus printMe...
-  //   btn.innerHTML = 'Click me and check the console!';
-  //   btn.onclick = printMe;
-
-  //   element.appendChild(btn);
 
   return element;
 }
